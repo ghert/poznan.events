39fe4b4e1ebc845dc89a9eceb318df79054dcddf
Filip Przydryga <fileepek@gmail.com>
2026-08-25 01:24:11 +0200

    Added las

--- lib/sources.ts
+++ lib/sources.ts
@@ -5,6 +5,7 @@ export const SCRAPE_INPUTS = [
   { url: 'https://www.facebook.com/FARBYWILDAPOZNAN/events', "upcoming_events_only": true },
   { url: 'https://www.facebook.com/schron44/events', "upcoming_events_only": true },
   { url: 'https://www.facebook.com/domtechnika/events', "upcoming_events_only": true },
+  { url: "https://www.facebook.com/las.poznan", "upcoming_events_only": true }
 ];
 
-export const VENUES = ['Blue Note Poznań', "Ślina", "Tama"];
+export const VENUES = ['Blue Note Poznań', "Ślina", "Tama", "LAS", "Dom", "Technika"];
