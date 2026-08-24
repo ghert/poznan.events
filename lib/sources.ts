86f4bb016b9b84e6403ccafe514edd46c7ce75e4
Filip Przydryga <fileepek@gmail.com>
2026-08-25 01:13:47 +0200

    Added new sources

--- lib/sources.ts
+++ lib/sources.ts
@@ -2,6 +2,9 @@ export const SCRAPE_INPUTS = [
   { url: 'https://www.facebook.com/bluenotepoznan/events', "upcoming_events_only": true },
   { url: 'https://www.facebook.com/slinamordo/events', "upcoming_events_only": true },
   { url: 'https://www.facebook.com/tamaklub/events', "upcoming_events_only": true },
+  { url: 'https://www.facebook.com/FARBYWILDAPOZNAN/events', "upcoming_events_only": true },
+  { url: 'https://www.facebook.com/schron44/events', "upcoming_events_only": true },
+  { url: 'https://www.facebook.com/domtechnika/events', "upcoming_events_only": true },
 ];
 
 export const VENUES = ['Blue Note Poznań', "Ślina", "Tama"];
