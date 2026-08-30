0496615f3a77097e5b8e4a85f82003dd9e437d4d
Filip Przydryga <fileepek@gmail.com>
2026-08-30 21:22:41 +0200

    Cache getting events

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import { sql } from '@/lib/db';
 import EventsList from "@/components/EventsList";
 import { ScrapedEventFromDB } from "@/lib/types";
+import { getEvents } from '@/lib/getEvents';
+
 
 export default async function Home() {
-  const events = await sql`
-    select * from events
-  `;
+  const events = await getEvents();
+
   return (
     <EventsList events={events as ScrapedEventFromDB[]} />
   );
--- lib/getEvents.ts
+++ lib/getEvents.ts
@@ -2,16 +2,13 @@ import { sql } from '@/lib/db';
 import { ScrapedEventFromDB } from './types';
 import { cacheLife } from 'next/cache';
 
-export async function getEvent(id: string): Promise<ScrapedEventFromDB | null> {
+export async function getEvents(): Promise<ScrapedEventFromDB[]> {
 
   'use cache';
   cacheLife('hours');
   const rows = await sql`
-    select *
-    from events
-    where id = ${id}
-    limit 1
+    select * from events where is_active and starts_at > now()
   `;
 
-  return rows[0] as ScrapedEventFromDB ?? null;
+  return rows as ScrapedEventFromDB[];
 }
